e0aba5f0ce6f431c7405ac56518fc6b4ea363afd
Gustavo Almeida <gustavo.almei2@hotmail.com>
2026-02-06 15:47:28 -0300

    restore file

--- infra/database.js
+++ infra/database.js
@@ -1,9 +1,16 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  let client;
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    ssl: getSSLValues(),
+  });
   try {
-    client = await getNewClient();
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -14,23 +21,8 @@ async function query(queryObject) {
   }
 }
 
-async function getNewClient() {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-  });
-
-  await client.connect();
-  return client;
-}
-
 export default {
   query: query,
-  getNewClient,
 };
 
 function getSSLValues() {
@@ -40,5 +32,5 @@ function getSSLValues() {
     };
   }
 
-  return process.env.NODE_ENV === "production" ? true : false;
+  return process.env.NODE_ENV === "development" ? false : true;
 }
