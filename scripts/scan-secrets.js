cdc6490f7d06c04ce9994e2061896f375d627db1
Gustavo Almeida <gustavo.almei2@hotmail.com>
2026-04-08 22:38:17 -0300

    ci: fix prettier in code

--- scripts/scan-secrets.js
+++ scripts/scan-secrets.js
@@ -1,17 +1,29 @@
-const { execSync } = require('child_process')
-const { spawnSync } = require('child_process')
+const { execSync } = require("child_process");
+const { spawnSync } = require("child_process");
 
-const stagedFiles = execSync('git diff --cached --name-only --diff-filter=ACM', {
-  encoding: 'utf-8',
-}).split('\n').filter(Boolean)
+const stagedFiles = execSync(
+  "git diff --cached --name-only --diff-filter=ACM",
+  {
+    encoding: "utf-8",
+  },
+)
+  .split("\n")
+  .filter(Boolean);
 
-
-const result = spawnSync('docker', [
-  'run', '--rm',
-  '--user', `${process.getuid()}:${process.getgid()}`,
-  '--volume', `${process.cwd()}:/usr/src/app`,
-  'lirantal/detect-secrets',
-  '--baseline', '.secrets.baseline',
-  ...stagedFiles
-], { stdio: 'inherit' })
-process.exit(result.status ?? 1)
+const result = spawnSync(
+  "docker",
+  [
+    "run",
+    "--rm",
+    "--user",
+    `${process.getuid()}:${process.getgid()}`,
+    "--volume",
+    `${process.cwd()}:/usr/src/app`,
+    "lirantal/detect-secrets",
+    "--baseline",
+    ".secrets.baseline",
+    ...stagedFiles,
+  ],
+  { stdio: "inherit" },
+);
+process.exit(result.status ?? 1);
