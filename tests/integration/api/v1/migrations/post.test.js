2dca0e142782d53f9834ffef113cdc593889ba7a
Gustavo Almeida <gustavo.almei2@hotmail.com>
2026-04-21 22:34:55 -0300

    style: run prettier:check and prettier:fix

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -9,9 +9,12 @@ describe("POST /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     describe("Running pending migrations", () => {
       test("For the first time", async () => {
-        const response = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
 
         expect(response.status).toBe(201);
         const responseBody = await response.json();
@@ -21,10 +24,12 @@ describe("POST /api/v1/migrations", () => {
       });
 
       test("For the second time", async () => {
-
-        const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
 
         expect(response2.status).toBe(200);
         const responseBody2 = await response2.json();
@@ -33,5 +38,5 @@ describe("POST /api/v1/migrations", () => {
         expect(responseBody2.length).toBe(0);
       });
     });
-  })
-})
+  });
+});
