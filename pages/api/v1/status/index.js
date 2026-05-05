b9674115b86b88432888b76ffe7cc6fc3c95affe
Gustavo Almeida <gustavo.almei2@hotmail.com>
2026-05-04 23:00:34 -0300

    feat: add customs errors on /migrations e /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,41 +1,36 @@
+import { createRouter } from "next-connect";
 import database from "infra/database";
-import { InternalServerError } from "infra/errors";
+import controller from "infra/controller";
 
-async function status(req, res) {
-  try {
-    const updatedAt = new Date().toISOString();
+const router = createRouter();
 
-    const versionPostgres = await database.query("show server_version;");
-    const versionPostgresValue = versionPostgres.rows[0].server_version;
-    const maxConectionsPostgres = await database.query("show max_connections;");
-    const maxConectionsPostgresValue =
-      maxConectionsPostgres.rows[0].max_connections;
-    const dataBaseName = process.env.POSTGRES_DB;
-    const openedConectionsPostgres = await database.query({
-      text: "select count(*)::int from pg_stat_activity where datname = $1;",
-      values: [dataBaseName],
-    });
-    const openedConectionsPostgresValue =
-      openedConectionsPostgres.rows[0].count;
-    res.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          max_connections: parseInt(maxConectionsPostgresValue),
-          opened_conections: openedConectionsPostgresValue,
-          version: versionPostgresValue,
-        },
+router.get(getHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function getHandler(req, res) {
+  const updatedAt = new Date().toISOString();
+
+  const versionPostgres = await database.query("show server_version;");
+  const versionPostgresValue = versionPostgres.rows[0].server_version;
+  const maxConectionsPostgres = await database.query("show max_connections;");
+  const maxConectionsPostgresValue =
+    maxConectionsPostgres.rows[0].max_connections;
+  const dataBaseName = process.env.POSTGRES_DB;
+  const openedConectionsPostgres = await database.query({
+    text: "select count(*)::int from pg_stat_activity where datname = $1;",
+    values: [dataBaseName],
+  });
+  const openedConectionsPostgresValue =
+    openedConectionsPostgres.rows[0].count;
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(maxConectionsPostgresValue),
+        opened_conections: openedConectionsPostgresValue,
+        version: versionPostgresValue,
       },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
-
-    console.log(`\n Erro dentro do catch do controller`);
-    console.error(publicErrorObject);
-    res.status(500).json(publicErrorObject);
-  }
+    },
+  });
 }
-
-export default status;
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -40,3 +40,61 @@ describe("POST /api/v1/migrations", () => {
     });
   });
 });
+describe("Method not allowed", () => {
+  describe("Method PUT", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "PUT",
+      });
+
+      expect(response.status).toBe(405);
+
+      const responsyBody = await response.json();
+
+      expect(responsyBody).toEqual({
+        name: "MethodNotAllowedError",
+        message: "Método não permitido para esse endpoint.",
+        action: "Verifique se o método HTTP enviado é válido para este endpoint.",
+        status_code: 405,
+      });
+    });
+  });
+
+  describe("Method DELETE", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "DELETE",
+      });
+
+      expect(response.status).toBe(405);
+
+      const responsyBody = await response.json();
+
+      expect(responsyBody).toEqual({
+        name: "MethodNotAllowedError",
+        message: "Método não permitido para esse endpoint.",
+        action: "Verifique se o método HTTP enviado é válido para este endpoint.",
+        status_code: 405,
+      });
+    });
+  });
+
+  describe("Method PATCH", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "PATCH",
+      });
+
+      expect(response.status).toBe(405);
+
+      const responsyBody = await response.json();
+
+      expect(responsyBody).toEqual({
+        name: "MethodNotAllowedError",
+        message: "Método não permitido para esse endpoint.",
+        action: "Verifique se o método HTTP enviado é válido para este endpoint.",
+        status_code: 405,
+      });
+    });
+  });
+});
