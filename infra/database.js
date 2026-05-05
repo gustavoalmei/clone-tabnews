90e99eb29e3f2a130382072120a53014d2efe7ae
Gustavo Almeida <gustavo.almei2@hotmail.com>
2026-05-04 23:01:56 -0300

    feat: use `next-connect` on `/migrations` and `/status`

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceServerError } from "infra/errors";
 
 async function query(queryObject) {
   let client;
@@ -7,9 +8,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log(`\n Erro dentro do catch do database.js`);
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceServerError({
+      cause: error,
+      message: "Erro na conexão com o banco ou na Query."
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
