021541a59ea4377e2d8d42fbd262dd275d544fb6
Gustavo Almeida <gustavo.almei2@hotmail.com>
2026-05-05 23:01:08 -0300

    feat: adicionar o model migrator responsável pela infra

---
+++ models/migrator.js
@@ -0,0 +1,48 @@
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+import database from "infra/database";
+
+const DefaultMigrations = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const peddingMigrations = await migrationRunner({
+      ...DefaultMigrations,
+      dbClient,
+    });
+    return peddingMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...DefaultMigrations,
+      dbClient,
+      dryRun: false,
+    });
+    return migratedMigrations;
+  } finally {
+    await dbClient.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations, runPendingMigrations
+}
+
+export default migrator;
