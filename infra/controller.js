77e1e80d37682a2382f3d339c76bb254546cc997
Gustavo Almeida <gustavo.almei2@hotmail.com>
2026-05-06 23:59:56 -0300

    feat: adicionar tratamento de erro para `ValidationError` no controlador

--- infra/controller.js
+++ infra/controller.js
@@ -1,4 +1,4 @@
-import { InternalServerError, MethodNotAllowedError } from "infra/errors";
+import { InternalServerError, MethodNotAllowedError, ValidationError } from "infra/errors";
 
 function onNoMatchHandler(req, res) {
   const publicErrorObject = new MethodNotAllowedError();
@@ -6,6 +6,10 @@ function onNoMatchHandler(req, res) {
 }
 
 function onErrorHandler(error, req, res) {
+  if (error instanceof ValidationError) {
+    return res.status(error.statusCode).json(error);
+  }
+
   const publicErrorObject = new InternalServerError({
     statusCode: error.statusCode,
     cause: error,
