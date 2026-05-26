123bf5b35c0ba1e206b8abd15f4d3ad0c72fb2a1
Gustavo Almeida <gustavo.mori@sesatech.com.br>
2026-05-25 23:31:57 -0300

    feat: add function set-cookie on controlelr

--- infra/controller.js
+++ infra/controller.js
@@ -6,6 +6,8 @@ import {
 
   UnauthorizedError,
 } from "infra/errors";
+import * as cookie from "cookie";
+import session from "models/session";
 
 function onNoMatchHandler(req, res) {
   const publicErrorObject = new MethodNotAllowedError();
@@ -28,10 +30,22 @@ function onErrorHandler(error, req, res) {
   console.error(publicErrorObject);
   res.status(publicErrorObject.statusCode).json(publicErrorObject);
 }
+
+async function setSessionCookie(res, sessionToken) {
+  const setCookie = cookie.serialize("session_id", sessionToken, {
+    path: "/", // informa qual caminho pode ter acesso ao cookie ( nesse caso todas as rotas)
+    // expires: new Date(newSession.expires_at), // informa a data de expiração
+    maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000, // informa o tempo de expiração em milisegundos
+    secure: process.env.NODE_ENV === "production", // informa se o cookie deve ser enviado apenas em requisições HTTPS
+    httpOnly: true, // informa se o cookie deve ser enviado apenas em requisições HTTP, um código JS não poderá acessar o cookie
+  });
+  res.setHeader("Set-Cookie", setCookie);
+}
 const controller = {
   errorHandlers: {
     onNoMatch: onNoMatchHandler,
     onError: onErrorHandler,
   },
+  setSessionCookie
 };
 export default controller;
