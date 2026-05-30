285203fbe2a65a949537129b48aca3ac093be775
Gustavo Almeida <gustavo.mori@sesatech.com.br>
2026-05-29 22:05:44 -0300

    feat: add new route to delete current session

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -6,6 +6,7 @@ import session from "models/session.js";
 const router = createRouter();
 
 router.post(postHandler);
+router.delete(deleteHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -22,4 +23,15 @@ async function postHandler(req, res) {
   controller.setSessionCookie(res, newSession.token);
 
   return res.status(201).json(newSession);
+}
+
+async function deleteHandler(req, res) {
+  const cookieSession = req.cookies.session_id;
+  const findSession = await session.findOneValidByToken(cookieSession);
+
+  const clearSession = await session.expireById(findSession.id);
+
+  controller.clearSessionCookie(res);
+
+  return res.status(401).json(clearSession)
 }
